cf5e65261d51a72207aacd3446357946e35f03c1
Exalter104 <muhammadsherazkhan104@gmail.com>
2024-05-29 13:12:03 +0500

    Dart Basics : Strings + Integers + Comments

--- bin/dart_basics.dart
+++ bin/dart_basics.dart
@@ -1,1 +1,26 @@
-void main() {}
+void main() {
+  //STRINGS.....................................................................
+
+  print("this is the String Value ");
+  print("2" "5");
+  print("2" * 4);
+
+  //INTEGER.....................................................................
+
+  print(2 + 3);
+  print("USING BODMAS RULE:" "${2 - 33 * (5 / 23)}");
+
+  //COMMENTS....................................................................
+  print("One Line Comment is");
+
+  // This is the Single Comments
+
+  /*
+  This is the 
+  Multi Line
+  Comments in Darts  
+  
+   */
+  print(
+      "One Line and Multi Line  Comment is ==> // **&**  This is the Multi Line Comments ==> /**/");
+}
