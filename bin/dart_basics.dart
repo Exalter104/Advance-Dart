5f6a4f067bae591e040ef2fe9471a3691b78ee0e
Exalter104 <muhammadsherazkhan104@gmail.com>
2024-05-31 12:53:11 +0500

    Dart Types : Var , Const , Final explain with example

--- bin/dart_basics.dart
+++ bin/dart_basics.dart
@@ -29,11 +29,67 @@ void main() {
    */
   print("One Line Comment is ==> // *&* This is the Multi Line Comments ==> /**/");
 */
-
+/*
 //VARIABLES....................................................................
 
-//Syntex :
+   Syntex : Datatype -- Varible Name = Value;
+   DateType : Int,Double,String,Bool ,var,const,final..etc
 
   int something = 34;
   print(something);
+  
+   */
+//VAR,FINAL,CONSTANT............................................................
+
+  /* 
+  
+  VAR :
+  "Var mean its value we change after at set onece ,but there value is
+  changing neither is data type"
+
+
+
+  // Example :
+
+  var x = 23;
+  x = 54; // there value is changed
+  //x = "54"; // there i an error bec we cannot changed their data type
+  print(x);
+
+  */
+
+  /* 
+  FINAL :
+  "Final is run type varaible mean if you give one value not reassign another value 
+  its data type or any number , but its value cannot set after one time set or the final
+  variable 'x' can only be set once.
+  "
+
+
+  
+  // Example :
+
+  final x = 23;
+  x = 54; // it got an error bec The final variable 'x' can only be set once.
+
+  print(x);
+
+  */
+
+  /* 
+  CONST :
+  "Const is compile type varaible mean before running  you should assign  a value
+  and its known value,Const  variable we cannot be reassign a value.
+  "
+
+
+  // Example :
+
+  const x =DateTime.now();// error bec its run type value we dont know his value 
+  x = 54; // it got an error bec Const "x"  variable we cannot be reassign a value.
+
+  print(x);
+
+
+   */
 }
