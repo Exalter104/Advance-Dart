3acc83075ec8997acf6943fceaf33dea32a5fc99
Exalter104 <muhammadsherazkhan104@gmail.com>
2024-06-26 06:10:55 +0500

    Throwing an Exception : With Breif explaination  + Example + Code analogy

--- bin/Conditions & loops/exception_handling.dart
+++ bin/Conditions & loops/exception_handling.dart
@@ -55,7 +55,7 @@ Syntex:
 try {
 .....
 }
-on Exception1(unsported error) {
+on Exception1(unsuported error) {
 // when an error is known , you know the error that what type of error is occurs
 }
 catch Exception2 {
@@ -65,8 +65,8 @@ finally {
 // code that should always execute whether an exception or not.
 }
 ................................................................................
- */
 
+ */
 //Example : "Finally & on" In Dart Try Catch
 // solution:
 
@@ -78,5 +78,65 @@ finally {
     print("catch : ${e.toString()}");
   } finally {
     print("Finally block is executed if exception is occurs or not");
+  }
+
+/*
+"**Throwing An Exception" In Dart Try Catch
+
+**Def:
+"A exception is a problem that occurs during the execution of program, so the "throw"
+keywords is "throw" this problem on the bases of problems is occurring or something
+wrong. when the exception is thrown its says "Something bad happen help to handle this"
+
+**Daily Life Example:
+
+Imagine you are a teacher in a classroom. You give your students a math test, and
+one of the questions is impossible to solve because it contains a mistake.
+
+**Normal Situation:
+Students solve the questions without any issues.
+
+**Exception Situation: 
+A student finds the impossible question and raises their hand
+to let you know about the problem. This is like throwing an exception.          
+
+**Code Analogy
+
+**Normal Execution:
+The teacher gives the test.
+Students work on it without any issues.
+
+**Throwing an Exception:
+The student finds the impossible question (a problem in the code).
+The student raises their hand (throws an exception).
+
+Syntex:
+keyword: "throw"
+try {
+.....
+}
+
+throw  Exception_name() {
+....
+}
+
+................................................................................
+ */
+
+//Example : "Throwing An Exception
+// solution:
+// main body
+  try {
+    checkAccount(-10);
+  } catch (e) {
+    print(e);
+  }
+}
+
+// function
+void checkAccount(int amount) {
+  if (amount < 0) {
+    throw FormatException(
+        " The ammount must be postive"); // you can pass error message
   }
 }
